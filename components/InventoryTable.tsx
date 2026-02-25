edf721a842f770b6caf7ba03edb652a3247d9475
ezraxl <112780637+informativelearning@users.noreply.github.com>
2026-02-25 21:03:18 +0000

    nshs

--- components/InventoryTable.tsx
+++ components/InventoryTable.tsx
@@ -80,14 +80,12 @@ const ProductCard = memo(({
             <img
               src={CORNER_WING}
               alt=""
-              // Made much larger (w-48/sm:w-56), adjusted offset to -top-2/-left-2, added object-contain
-              className="absolute -top-2 -left-2 w-48 h-48 sm:w-56 sm:h-56 object-contain pointer-events-none z-[1] invert opacity-40 mix-blend-screen"
+              className="absolute -top-2 -left-2 w-48 h-48 sm:w-56 sm:h-56 object-contain pointer-events-none z-[1] opacity-40"
             />
             <img
               src={CORNER_WING}
               alt=""
-              // Made much larger, adjusted offset to -bottom-2/-right-2, added object-contain
-              className="absolute -bottom-2 -right-2 w-48 h-48 sm:w-56 sm:h-56 object-contain pointer-events-none z-[1] invert opacity-40 mix-blend-screen rotate-180"
+              className="absolute -bottom-2 -right-2 w-48 h-48 sm:w-56 sm:h-56 object-contain pointer-events-none z-[1] opacity-40 rotate-180"
             />
             <div className="relative z-10 flex flex-col items-center">
               <span className="serif italic text-2xl sm:text-3xl text-white/40 leading-none">No Image</span>
